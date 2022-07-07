832302cdb81aa3f0667b8c97545737f263ccc173
Vampire <102051223+HotVampire@users.noreply.github.com>
2022-07-07 15:41:32 +0300

    5
    Added new screens + fixed navbar + added fixed navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
   ],
   darkMode: 'media',
   theme: {
+    screens: {
+      'sm': '440px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+  },
     extend: {},
   },
   plugins: [
