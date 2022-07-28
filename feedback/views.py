fe3b01140b8f8c2ed8e7ecffbd27bb37f88eca65
Pngroot <104679324+Pngroot@users.noreply.github.com>
2022-07-28 23:25:03 +0300

    Corrected the POST method for the FeedbackView class and added messages for incorrect data entry

--- feedback/views.py
+++ feedback/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import redirect
 from django.core.validators import validate_email
+from django.core.validators import ValidationError
 from .forms import FeedbackForm
 from django.views.generic.edit import CreateView
 from django.contrib import messages
 
-if_email = False
-if_message = False
-
 
 class FeedbackView(CreateView):
+    email_message = None
+
     def post(self, request, **kwargs):
         form = FeedbackForm(self.request.POST)
         if form.is_valid():
             last_form = form.save(commit=False)
             last_form.save()
+        else:
+            data = form.data
+            email = data.get('email')
+            message = data.get('message')
+            try:
+                validate_email(email)
+            except ValidationError:
+                messages.error(request, 'Incorrect email!')
+            if not message:
+                messages.warning(request, 'Required field!')
         return redirect('/')
